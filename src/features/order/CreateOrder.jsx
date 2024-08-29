cdb6ac8905ae94ca4b749b7bce79a09bbfc250ec
RamezCh <ramiz_chreide@outlook.com>
2024-08-30 01:57:33 +0300

    Reusing styles with @apply

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -47,13 +47,19 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" defaultValue={username} required />
+          <input
+            className="input"
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div>
           <label>Phone number</label>
           <div className="grow">
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
             {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
         </div>
@@ -62,7 +68,7 @@ function CreateOrder() {
           <label>Address</label>
           <div>
             <input
-              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              className="input"
               type="text"
               name="address"
               disabled={isLoadingAddress}
