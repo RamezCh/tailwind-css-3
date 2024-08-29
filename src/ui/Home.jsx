35f0cf7626f7d6e2e9aae87cad6532e22163c8bb
RamezCh <ramiz_chreide@outlook.com>
2024-08-30 00:49:34 +0300

    Using Flex

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 
   return (
     <div className="my-10 text-center sm:my-16">
-      <h1 className="mb-8 text-xl font-semibold">
+      <h1 className="mb-8 px-4 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
