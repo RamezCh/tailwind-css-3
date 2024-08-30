353a649af5119d44c723047bdeef9a674d03d841
RamezCh <ramiz_chreide@outlook.com>
2024-08-30 22:51:59 +0300

    Absolute Positioning, Z-index and more

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,8 @@
 function Loader() {
+  // inset-0 means top,left, right, bot 0px
   return (
-    <div className="">
-      <div className=""></div>
+    <div className="backdrop-blue-sm absolute inset-0 flex items-center justify-center bg-slate-200/20">
+      <div className="loader"></div>
     </div>
   );
 }
